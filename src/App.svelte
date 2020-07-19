54acaeb51165a17e57357b73b87afefb42f66021
Thiago Zanetti <thiago.project@gmail.com>
2020-07-19 17:44:21 -0300

    bootstrapping the app

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<main>
+	<section>
+		<h1>We are teelabs.</h1>
+		<h3>coming soon.</h3>
+	</section>
+</main>
+
+<style>
+	main {
+    display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	h1 {
+		display: block;
+		font-size: 64px;
+		padding: 0 0.2em;
+		margin-left: 2em;
+		margin-top: -2em;
+	}
+
+	h3 {
+		display: block;
+		font-size: 32px;
+		padding: 0 0.2em;
+		margin-left: 12.53em;
+		margin-top: -1.8em;
+	}
+</style>
